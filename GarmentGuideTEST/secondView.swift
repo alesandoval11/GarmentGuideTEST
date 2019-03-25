696916173ef3a8f70b8414132ce676b31f55fc16
Alejandra Sandoval <alesandoval11@tamu.edu>
2019-03-25 12:36:58 -0500

    vMarch252

--- GarmentGuideTEST/secondView.swift
+++ GarmentGuideTEST/secondView.swift
@@ -9,16 +9,52 @@ import UIKit
 
 class secondView: UIViewController {
     
+    var firstLabelButton:String = ""
+    var secondLabelButton:String = ""
+    var thirdLabelButton:String = ""
+    var fourthLabelButton:String = ""
+    var fifthLabelButton:String = ""
+   
  
+
+    @IBOutlet weak var firstLabel: UIButton!
+    @IBOutlet weak var secondLabel: UIButton!
+    @IBOutlet weak var thirdLabel: UIButton!
+    @IBOutlet weak var fourthLabel: UIButton!
+    @IBOutlet weak var fifthLabel: UIButton!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        //Changes the labels of the buttons to whatever was sent from the JSON
+        firstLabel.setTitle( firstLabelButton , for: .normal )
+        secondLabel.setTitle( secondLabelButton , for: .normal )
+        thirdLabel.setTitle( thirdLabelButton , for: .normal )
+        fourthLabel.setTitle( fourthLabelButton , for: .normal )
+        fifthLabel.setTitle( fifthLabelButton , for: .normal )
+    }
+    
+
+   
     @IBAction func backHome2(_ sender: UIButton) {
         self.performSegue(withIdentifier: "home2", sender: self)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Action of Buttons
+    @IBAction func firstButtonAct(_ sender: Any) {
+        print("Button for \(firstLabelButton) pressed")
     }
-    
-    
+    @IBAction func secondButtonAct(_ sender: UIButton) {
+       print("Button for \(secondLabelButton) pressed")
+    }
+    @IBAction func thirdButtonAct(_ sender: UIButton) {
+        print("Button for \(thirdLabelButton) pressed")
+    }
+    @IBAction func fourthButtonAct(_ sender: UIButton) {
+            print("Button for \(fourthLabelButton) pressed")
+    }
+    @IBAction func fifthButtonAct(_ sender: Any) {
+         print("Button for \(fifthLabelButton) pressed")
+    }
 }
 
---
+++ ios/Pods/Target Support Files/Pods-BeaconServiceDemoApp/Pods-BeaconServiceDemoApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BeaconServiceDemoApp : NSObject
+@end
+@implementation PodsDummy_Pods_BeaconServiceDemoApp
+@end
