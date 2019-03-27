77acd6c65ed68716f9887fe7a9cc31f0273a7ab6
raykim1619 <raykim1619@tamu.edu>
2019-03-26 23:30:44 -0500

    Fixed formatting and exclude 127 dB measurement

--- GarmentGuideTEST/scannerView.swift
+++ GarmentGuideTEST/scannerView.swift
@@ -30,6 +30,23 @@ class scannerView: UIViewController, UITextFieldDelegate, BeaconScannerDelegate 
             for b in availableBeacons{
                 self.textField.text?.append(b.name + "\t" + String(b.RSSI) + " dB" + "\n")
             }
+            
+            if availableBeacons.count == 0 {
+                let n = stats.count
+                let max = stats.max()!
+                let min = stats.min()!
+                let sum = stats.reduce(0,+)
+                var mean = sum/n
+                let countedSet = NSCountedSet(array: stats)
+                let mostFrequent: Int = countedSet.max { countedSet.count(for: $0) < countedSet.count(for: $1) } as! Int
+                stats.removeAll(keepingCapacity: false)
+                DispatchQueue.main.async {
+                    self.textField.text? = ""
+                    self.textField!.text?.append("Max:\t" + String(max) + " dB\nMin:   \t" + String(min) + " dB\nMean:\t")
+                    self.textField.text?.append(String(mean) + " dB\nMode:\t" + String(mostFrequent) + " dB\nN:      \t" + String(n))
+                }
+                
+            }
         }
     }
     
@@ -49,9 +66,12 @@ class scannerView: UIViewController, UITextFieldDelegate, BeaconScannerDelegate 
         printTable()
     }
     func didUpdateBeacon(beaconScanner: BeaconScanner, beaconInfo: BeaconInfo) {
-        for i in 0...(availableBeacons.count - 1){
-            if availableBeacons[i].id == beaconInfo.beaconID.description{
-                availableBeacons[i].RSSI = beaconInfo.RSSI
+        if beaconInfo.RSSI != 127 {
+            for i in 0...(availableBeacons.count - 1){
+                if availableBeacons[i].id == beaconInfo.beaconID.description{
+                    availableBeacons[i].RSSI = beaconInfo.RSSI
+                    stats.append(Int(availableBeacons[i].RSSI)) // debug
+                }
             }
         }
         printTable()
@@ -88,6 +108,7 @@ var beaconNames =   ["f7826da6bc5b71e0893e716e687a6a41": "Beacon1",
                      "f7826da6bc5b71e0893e37796e48706b": "Beacon4"]
 
 var availableBeacons = [ParsedBeacon]()
+var stats = [Int]() // debug
 
 class ParsedBeacon {
     let name: String
