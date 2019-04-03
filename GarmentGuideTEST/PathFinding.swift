55bf88675d2c397796748b877c7ba063fee39ced
raykim1619 <raykim1619@tamu.edu>
2019-04-02 22:45:37 -0500

    Fixed dictionary of nodes [coordinates: Node]. Created function structure of a star algorithm

--- GarmentGuideTEST/PathFinding.swift
+++ GarmentGuideTEST/PathFinding.swift
@@ -11,45 +11,55 @@ import Foundation
 class Node {
     let id: Int
     let coordinates: [Int]
-    let connections: [Connection]
+    var neighbors: [Node]
     
-    init(id: Int, coordinates: [Int], connections: [Connection]) {
+    init(id: Int, coordinates: [Int]) {
         self.id = id
         self.coordinates = coordinates
-        self.connections = connections
+        self.neighbors = []
     }
 }
 
-class Connection {
-    let to: Node
-    let weight: Int
-    
-    init(to: Node) {
-        self.to = to
-        self.weight = 0
-    }
-}
-
-func readJSON(fileName: String, fileType: String){
+/**
+ Generates nodes from a JSON file
+ - Parameters:
+    fileName: The name of the file
+    fileType: The type of file
+ - Returns: Dictionary of Nodes
+ */
+func createNodes(fileName: String, fileType: String)->[[Int]:Node]{
+    var nodeDict: [[Int]: Node] = [:]  //Holds all the nodes.
     if let path = Bundle.main.path(forResource: fileName, ofType: fileType) {
         do {
             let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
             let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
             if let jsonResult = jsonResult as? Dictionary<String, AnyObject>, let building = jsonResult["EABA"] as? [Any] {
-                for node in building as! [Dictionary<String, AnyObject>] { // or [[String:AnyObject]]
+                for node in building as! [[String: AnyObject]] {
                     let id = node["id"] as! Int
-                    let label = node["label"] as! String
                     let coordinates = node["coordinates"] as! [Int]
-                    let connections = node["connections"] as! [Int]
-                    print(id)
-                    print(label)
-                    print(coordinates)
-                    print(connections)
+                    nodeDict.updateValue(Node(id: id,coordinates: coordinates), forKey: node["coordinates"] as! [Int])
                 }
+                for node in building as! [[String: AnyObject]] {
+                    let neighbors = node["connections"] as! [[Int]]
+                    for neighbor in neighbors {
+                        nodeDict[node["coordinates"] as! [Int]]!.neighbors.append(nodeDict[neighbor]!)
+                    }
+                }
+                dump(nodeDict)
             }
         } catch {
             // handle error
             print("Error")
         }
+    }
+     return nodeDict
+}
+
+func AStar(nodeDict: [[Int]: Node], start: [Int], end:[Int]) {
+    if (nodeDict[start] != nil) {            //If user is located at existing node
+        //A star
+    }
+    else {
+        //Zone guidance function
     }
 }
